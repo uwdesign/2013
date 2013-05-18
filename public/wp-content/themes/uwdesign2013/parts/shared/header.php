c960e58b61347f8a8c2195557cf52c12b68f8bf3
Tom Conroy <tom@conroy.com.au>
2013-05-17 23:00:29 -0700

    Header grid

--- public/wp-content/themes/uwdesign2013/parts/shared/header.php
+++ public/wp-content/themes/uwdesign2013/parts/shared/header.php
@@ -1,9 +1,11 @@
 <div id="header-wrapper">
   <div id="header">
-    <div id="header-inner">
-      <h1 id="site-title"><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-      <?php wp_nav_menu(); ?>
-    </div>  
+    <div id="header-inner-wrapper">
+      <div id="header-inner">
+        <h1 id="site-title"><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+        <?php wp_nav_menu(); ?>
+      </div>
+    </div>
   </div>
 
   <div id="designers-menu" class="sub-menu-wrapper">
--- public/wp-content/themes/uwdesign2013/parts/shared/work-menu.php
+++ public/wp-content/themes/uwdesign2013/parts/shared/work-menu.php
@@ -1,11 +1,13 @@
-<div id="work-menu-title">
-  <h2>Work</h2>
-  <p>Sort by Media Type</p>
-</div>
+<div id="work-menu-wrapper">
+  <div id="work-menu-title">
+    <h2>Work</h2>
+    <p>Sort by Media Type</p>
+  </div>
 
-<ul class="categories">
-  <?php wp_list_categories( array(
-    'title_li' => '',
-    'hide_empty' => 0
-  ) ); ?>
-</ul>
+  <ul class="categories">
+    <?php wp_list_categories( array(
+      'title_li' => '',
+      'hide_empty' => 0
+    ) ); ?>
+  </ul>
+</div>
