f1fe6224b76c88e95f9734580f15f8a23b5ef8e2
Tom Conroy <tom@conroy.com.au>
2013-05-19 14:20:32 -0700

    Fix author header

--- public/wp-content/themes/uwdesign2013/author.php
+++ public/wp-content/themes/uwdesign2013/author.php
@@ -10,40 +10,43 @@
   <div class="designer-single">
     
     <div class="designer-single__info">
-      <div class="designer-single__name">
-        <h2><?php echo get_the_author(); ?></h2>
-        <?php the_program_link($post->post_author, 'description'); ?>
-      </div>
       
-      <div class="designer-single__posts-list">
-        <h5>Projects</h5>
-        <?php while(have_posts()): the_post(); ?>
-          <a href="#post-<?php the_ID(); ?>"><?php the_title(); ?></a>
-        <?php endwhile; rewind_posts(); ?>
-      </div>
+      <div class="designer-single__info-wrapper">
       
-      <?php $portfolio_link = get_cimyFieldValue($post->post_author, 'portfolio'); ?>
-      <?php $email_link = get_cimyFieldValue($post->post_author, 'email'); ?>
+        <div class="designer-single__name">
+          <h2><?php echo get_the_author(); ?></h2>
+          <?php the_program_link($post->post_author, 'description'); ?>
+        </div>
       
-      <div class="designer-single__portfolio">
-        <?php if( $portfolio_link != '' || $email_link != '' ): ?>
-          <h5>Learn More</h5>
-          <?php if( $portfolio_link != '' ): ?>
-            <a href="<?php echo $portfolio_link; ?>">View Portfolio</a>
+        <div class="designer-single__posts-list">
+          <h5>Projects</h5>
+          <?php while(have_posts()): the_post(); ?>
+            <a href="#post-<?php the_ID(); ?>"><?php the_title(); ?></a>
+          <?php endwhile; rewind_posts(); ?>
+        </div>
+      
+        <?php $portfolio_link = get_cimyFieldValue($post->post_author, 'portfolio'); ?>
+        <?php $email_link = get_cimyFieldValue($post->post_author, 'email'); ?>
+      
+        <div class="designer-single__portfolio">
+          <?php if( $portfolio_link != '' || $email_link != '' ): ?>
+            <h5>Learn More</h5>
+            <?php if( $portfolio_link != '' ): ?>
+              <a href="<?php echo $portfolio_link; ?>">View Portfolio</a>
+            <?php endif; ?>
+            <?php if( $email_link != '' ): ?>
+              <a href="<?php echo $portfolio_link; ?>">Email</a>
+            <?php endif; ?>
           <?php endif; ?>
-          <?php if( $email_link != '' ): ?>
-            <a href="<?php echo $portfolio_link; ?>">Email</a>
-          <?php endif; ?>
-        <?php endif; ?>
-      </div>
+        </div>
       
-      <div class="designer-single__headshot">
-        <?php the_headshot($post->post_author); ?>
+        <div class="designer-single__headshot">
+          <?php the_headshot($post->post_author); ?>
+        </div>
+      
       </div>
       
     </div>
-    
-    <hr class="designer-single__info-border" />
     
     <div class="designer-single__posts">
       <?php while(have_posts()): the_post(); ?>
