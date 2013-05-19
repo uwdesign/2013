65eb5a37586aaee108127e5465e4aca2080cbf08
Tom Conroy <tom@conroy.com.au>
2013-05-19 00:46:18 -0700

    Coauthors, media styles

--- public/wp-content/themes/uwdesign2013/parts/post-single.php
+++ public/wp-content/themes/uwdesign2013/parts/post-single.php
@@ -3,19 +3,30 @@
   <div class="post-single__info">
     
     <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>      
+    
+    <?php the_content(); ?>
+    
+    <hr />
     
     <?php $coauthors = get_the_other_coauthors($post->post_author); ?>
 	  
     <?php if( count($coauthors) ): ?>
-      
-      <h5>Team Members</h5>
-      
-      <?php foreach($coauthors as $coauthor): ?>
-        <a href="<?php echo get_author_permalink($coauthor->ID); ?>"><?php echo $coauthor->display_name; ?></a>
-      <?php endforeach; ?>
-    
+      <div class="post-single__info-section">
+        <h5>Team Members</h5>
+        <ul>
+          <?php foreach($coauthors as $coauthor): ?>
+            <li><a href="<?php echo get_author_permalink($coauthor->ID); ?>"><?php echo $coauthor->display_name; ?></a></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
     <?php endif; ?>
+    
+    <div class="post-single__info-section">
+      <h5>Media</h5>
+      <?php the_category(); ?>
+    </div>
+    
+    <hr />
     
   </div>
 
