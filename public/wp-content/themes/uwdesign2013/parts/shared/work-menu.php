84992166970193c063370f60025a43173376e146
Tom Conroy <tom@conroy.com.au>
2013-05-22 22:57:49 -0700

    Work menu updates

--- public/wp-content/themes/uwdesign2013/parts/shared/work-menu.php
+++ public/wp-content/themes/uwdesign2013/parts/shared/work-menu.php
@@ -1,10 +1,10 @@
 <div id="work-menu-wrapper">
   <div id="work-menu-title">
     <h2>Work</h2>
-    <p><a href="<?php echo get_permalink( get_page_by_path( 'work' ) ); ?>">View All</a></p>
   </div>
 
   <ul class="categories">
+    <li class="all"><a href="<?php echo get_permalink( get_page_by_path( 'work' ) ); ?>">View All</a></li>
     <?php wp_list_categories( array(
       'title_li' => '',
       'hide_empty' => 0
